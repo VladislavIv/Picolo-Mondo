e876254a04a5ab774aedc4eee8eb150ed3053fcb
Vladislav Ivchenko <vladiv55555@gmail.com>
2020-09-26 16:37:44 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slide_container').slick({
+        arrows: true,
+        dots: true,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnDotsHover: true,
+        pauseOnFocus: true,
+        pauseOnHover: true
+    });
+});
